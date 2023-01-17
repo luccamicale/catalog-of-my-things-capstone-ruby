353c6a458a366b3a586ef9e3d230f01788624bde
luccamicale <luccamicale2015@gmail.com>
2023-01-17 17:11:01 -0300

    implement the rspec test for genre file

---
+++ spec/genre_spec.rb
@@ -0,0 +1,21 @@
+require_relative 'spec_helper'
+
+describe "Genre class unit tests" do
+  before(:each) do
+    @genre = Genre.new('Reggaeton')
+  end
+
+  context 'New Genre object' do
+    it 'Takes one parameter and returns a Genre object' do
+      expect(@genre).to be_instance_of(Genre)
+    end
+
+    it 'the correct name is Reggaeton' do
+      expect(@genre.name).to eq('Reggaeton')
+    end
+
+    it 'return a string' do
+      expect(@genre.name).to be_kind_of(String)
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # Uncomment the following lines when you create the classes' spec files
 
-# require_relative '../src/classes/genre'
+require_relative '../src/classes/genre'
 # require_relative '../src/classes/music_album'
 # require_relative '../src/classes/author'
 # require_relative '../src/classes/game'
