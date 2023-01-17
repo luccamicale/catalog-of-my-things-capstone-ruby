5b7d42f349e8298ed6799af7e36ef4095e47fad5
Camilovelag <camilo.vela.galindo@gmail.com>
2023-01-17 13:44:07 -0500

    Add test to validate if book is added correctly

---
+++ spec/book_spec.rb
@@ -0,0 +1,28 @@
+require './src/classes/book'
+# use require_relative 'spec_helper' when running correctly
+
+describe Book do
+  context 'check if book is added correctly' do
+    it 'should be able to add book' do
+      book = Book.new('2018-01-01', 'good')
+      book.genre = 'Horror'
+      book.author = 'Stephen King'
+      book.label = 'Penguin'
+
+      expect(book.genre).to eq('Horror')
+      expect(book.author).to eq('Stephen King')
+      expect(book.label).to eq('Penguin')
+      expect(book.publish_date).to eq('2018-01-01')
+      expect(book.archived).to eq(false)
+    end
+  end
+
+  context 'check if book is archived correctly' do
+    it 'should be able to archive book' do
+      book = Book.new('2000-01-01', 'bad')
+      book.move_to_archive
+
+      expect(book.archived).to eq(true)
+    end
+  end
+end
--- src/classes/book.rb
+++ src/classes/book.rb
@@ -1,12 +1,15 @@
 require_relative 'item'
 
 class Book < Item
-  def initialize(publish_date)
+  attr_accessor :publish_date, :cover_state
+
+  def initialize(publish_date, cover_state)
     super()
     @publish_date = publish_date
+    @cover_state = cover_state
   end
 
   def can_be_archived?
-    super && (Date.today - Date.parse(@publish_date)) / 365 > 5
+    super && @cover_state == 'bad'
   end
 end
