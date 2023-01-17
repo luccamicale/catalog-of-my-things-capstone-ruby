01a388fc126cc26ab17d941057c98fa025e9b125
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-01-17 11:06:37 -0800

    Day 1 game label/create classes

--- src/classes/author.rb
+++ src/classes/author.rb
@@ -1,1 +1,17 @@
 # Author class
+class Author
+    attr_reader :id, :first_name, :last_name
+    attr_accessor :items
+
+    def initialize(first_name, last_name)
+      @id = rand(1..1000)
+      @first_name = first_name
+      @last_name = last_name
+      @items = []
+    end
+
+    def add_item(item)
+      @items << item
+      item.add_author(self)
+    end
+  end
--- src/classes/game.rb
+++ src/classes/game.rb
@@ -1,1 +1,17 @@
 # Game class
+require_relative './item'
+require 'date'
+class Game < Item
+  attr_reader :multiplayer, :last_played_at
+
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @id = rand(1..1000)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && (DateTime.now - Date.parse(last_played_at)).to_i / 365 > 2
+  end
+end
