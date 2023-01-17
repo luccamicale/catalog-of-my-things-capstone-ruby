a2d477c1ea25b0d427a4ecf3641be28ee6321736
luccamicale <luccamicale2015@gmail.com>
2023-01-17 14:20:08 -0300

    install rspec and create files xxx_spec.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require_relative '../src/classes/genre'
+require_relative '../src/classes/music_album'
+require_relative '../src/classes/author'
+require_relative '../src/classes/book'
+require_relative '../src/classes/game'
+require_relative '../src/classes/label'
+require_relative '../src/classes/item'
