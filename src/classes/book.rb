9b734292c8c74f731de23a81dd7857279b67d208
Camilovelag <camilo.vela.galindo@gmail.com>
2023-01-17 12:59:13 -0500

    Create book class

--- src/classes/book.rb
+++ src/classes/book.rb
@@ -1,1 +1,12 @@
-# Book class
+require_relative 'item'
+
+class Book < Item
+  def initialize(publish_date)
+    super()
+    @publish_date = publish_date
+  end
+
+  def can_be_archived?
+    super && (Date.today - Date.parse(@publish_date)) / 365 > 5
+  end
+end
