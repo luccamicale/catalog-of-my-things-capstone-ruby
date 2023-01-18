202c155b47217ed83e8cc9f885d1d25e006941ea
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-01-18 04:42:11 -0800

    Implement-game-methods-and-unit-testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 # Uncomment the following lines when you create the classes' spec files
 
-# require_relative '../src/classes/genre'
-# require_relative '../src/classes/music_album'
+require_relative '../src/classes/genre'
+require_relative '../src/classes/music_album'
 require_relative '../src/classes/author'
 require_relative '../src/classes/game'
 require_relative '../src/classes/item'
-# require_relative '../src/classes/book'
-# require_relative '../src/classes/label'
+require_relative '../src/classes/book'
+require_relative '../src/classes/label'
